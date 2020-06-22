48ae04262c43a54ab46cdc79b30b059d546ff610
LWRGitHub <loganreynoldslwr@gmail.com>
2020-06-22 15:34:27 -0700

    Results.js page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Jumbotron } from 'react-bootstrap'
 import SearchForm from './components/SearchForm';
+import Results from './components/Results';
 
 class App extends Component{
   render(){
@@ -14,6 +15,7 @@ class App extends Component{
             <SearchForm />
           </Container>
         </Jumbotron>
+        <Results />
       </div>
     );
   }
